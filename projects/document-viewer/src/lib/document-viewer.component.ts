28a10c03db5a333829aee5b38f990f7a5c992e70
BLEU Koty <bleukoty@gmail.com>
2020-05-09 20:03:29 +0000

    Amélioration du composant

--- projects/document-viewer/src/lib/document-viewer.component.ts
+++ projects/document-viewer/src/lib/document-viewer.component.ts
@@ -17,19 +17,9 @@ export class DocumentViewerComponent implements OnInit {
 
   // Displays folders or documents
   displayMode: DisplayMode;
-  navigationButtonToogleValues = [ '<i class="fas fa-arrow-alt-circle-left"></i> Liste des dossiers',
-                                   'Dossier actif <i class="fas fa-arrow-alt-circle-right"></i>'];
-  filterInputToogleValues = ['Rechercher un document...', 'Rechercher un dossier...'];
-
-  navigationButtonText = 'Liste des dossiers';
-  placeholder = 'Rechercher un document...';
 
   folderId = 'N° CCC-VVV';
   docType = 'Pièce d\'identité';
-
-  documentList = ['Carte d\'identité - Recto', 'Carte d\'identité - Verso', 'formulaire - 1'];
-
-  iconLeft = true;
 
 
   constructor() { }
